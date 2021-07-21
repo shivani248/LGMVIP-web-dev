02aebe8745732f90c4cd37c14cdbc138bed773ab
shivani248 <56289002+shivani248@users.noreply.github.com>
2021-07-21 19:02:56 +0530

    Add files via upload

---
+++ task1/task.js
@@ -0,0 +1,16 @@
+function submitted()
+       {
+           var str=document.getElementById("text").Value;
+          
+           alert("singned successfully with email"+str);
+       }
+
+       function funVideo(video)
+       {
+           document.getElementById("slider").src=video;
+       }
+
+function Funalbum(image)
+ {
+ document.getElementById("alimg").src=image;
+}
